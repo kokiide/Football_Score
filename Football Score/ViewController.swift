a0e86433f8738988c7c9cc69c8f2ab901766b1f0
Koki.Ide <s15087ki@sfc.keio.ac.jp>
2017-09-03 03:55:01 +0900

    initial

--- Football Score/ViewController.swift
+++ Football Score/ViewController.swift
@@ -9,11 +9,35 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         // Do any additional setup after loading the view, typically from a nib.
+        self.view.backgroundColor = UIColor(red: 0.032, green: 0.240, blue: 1, alpha: 1.0)
+        let yourtmTF = UITextField()
+        yourtmTF.text = "add the name of your club or team."
+        yourtmTF.font = UIFont.boldSystemFont(ofSize: 16.0)
+        yourtmTF.sizeToFit()
+        yourtmTF.textColor = UIColor(red: 0.1608, green: 0, blue: 0.1804, alpha: 1.0)
+        yourtmTF.centerXAnchor = self.view.centerXAnchor + 60
+        yourtmTF.centerYAnchor = self.view.centerYAnchor
+        self.view.addSubview(yourtmTF)
+        
+        
+        let oppotmTF = UITextField()
+        oppotmTF.text = "add the name of opposition club or team."
+        oppotmTF.font = UIFont.boldSystemFont(ofSize: 16.0)
+        oppotmTF.sizeToFit()
+        oppotmTF.textColor = UIColor(red: 0.1608, green: 0, blue: 0.1804, alpha: 1.0)
+        oppotmTF.center = self.view.center
+        self.view.addSubview(oppotmTF)
+
     }
+    
+    
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
